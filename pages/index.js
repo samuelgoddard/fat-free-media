7fae8633c8f87fae785686b7a5554c658008c23e
Sam Goddard <hello@samgoddard.co.uk>
2021-01-28 01:19:35 +0000

    first pass at BIG X

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,9 @@ export default function Index({ subscription }) {
           exit="exit"
         >
           <motion.div variants={fade} className="mb-16 md:mb-20 pt-32 md:pt-48 xl:pt-56">
+            <svg className="bg-x z-0 opacity-75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2293.844 2243.667"><path fill="#ffffff" d="M920.549 960.027c-24.395-206.845-47.501-403.144-70.775-599.46-3.456-29.083-11.867-58.246-10.761-87.096 3.578-93.446 76.297-139.8 159.336-97.5 87.963 44.782 171.734 97.798 264.394 151.295 23.875 143.713 48.933 294.62 75.851 456.633 164.336-68.087 320.902-132.974 474.662-196.673l313.656 218.977c-9.165 90.961-53.964 139.546-129.478 168.1-181.596 68.664-362.23 139.903-543.19 210.34-20.165 7.864-39.744 17.188-70.067 30.395 4.433 41.673 6.69 84.108 13.702 125.738 40.718 242.03 83.188 483.77 123.316 725.893 5.61 33.799 1.604 69.17 2.412 132.113-164.3-80.74-306.613-150.65-438.69-215.568-37.16-210.437-70.797-400.911-106.349-602.23-199.494 78.834-391.113 154.555-578.868 228.755-128.321-86.98-244.42-165.678-383.043-259.626 307.057-162.055 609.898-256.18 903.892-390.086z" data-name="Path 1"/></svg>
             <Container thin>
-              <div className="w-full mb-12 md:mb-16 xl:mb-24">
+              <div className="w-full mb-12 md:mb-16 xl:mb-24 relative z-10">
                 <div className="mb-5 md:mb-8">
                   <div className="flex mb-2">
                     <div className="w-5 h-5 bg-off-black rounded-full"></div>
@@ -44,7 +45,7 @@ export default function Index({ subscription }) {
             </Container>
 
             <Container bleed>
-              <div className="w-full mb-16 xl:mb-24">
+              <div className="w-full mb-16 xl:mb-24 relative z-10">
                 <div className="mb-4 md:mb-6 text-right px-6 md:px-0">
                   <Link href="/"><a aria-label="Navigate to Our Work" className="underline tracking-tight text-lg inline-block items-center">
                     <div className="flex items-center">
@@ -65,7 +66,7 @@ export default function Index({ subscription }) {
             </Container>
 
             <Container thin>
-              <div className="w-full mb-12 md:mb-16 xl:mb-24">
+              <div className="w-full mb-12 md:mb-16 xl:mb-24 relative z-10">
                 <div className="flex flex-wrap">
                   <div className="w-full md:w-full max-w-sm">
                     <span className="block text-xs uppercase tracking-tighter leading-none">Information —</span>
@@ -80,7 +81,7 @@ export default function Index({ subscription }) {
                 </div>
               </div>
 
-              <div className="w-full mb-12 md:mb-16 xl:mb-24">
+              <div className="w-full mb-12 md:mb-16 xl:mb-24 relative z-10">
                 <div className="flex flex-wrap md:-mx-8">
                   <div className="w-full md:w-1/3 md:px-8">
                     <div className="h-full py-6 md:py-10 border-b md:border-b-0 md:border-l border-dotted border-off-black border-opacity-50 md:px-8">
--- pages/menu.js
+++ pages/menu.js
@@ -25,11 +25,13 @@ export default function Menu({ subscription }) {
           initial="initial"
           animate="enter"
           exit="exit"
-          className=""
+
         >
           <motion.div variants={fade} className="pt-32 md:pt-48 xl:pt-56 bg-orange text-off-white min-h-screen flex flex-wrap items-center md:items-end">
+            <svg className="bg-x z-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2293.844 2243.667"><path fill="#ce8860" d="M920.549 960.027c-24.395-206.845-47.501-403.144-70.775-599.46-3.456-29.083-11.867-58.246-10.761-87.096 3.578-93.446 76.297-139.8 159.336-97.5 87.963 44.782 171.734 97.798 264.394 151.295 23.875 143.713 48.933 294.62 75.851 456.633 164.336-68.087 320.902-132.974 474.662-196.673l313.656 218.977c-9.165 90.961-53.964 139.546-129.478 168.1-181.596 68.664-362.23 139.903-543.19 210.34-20.165 7.864-39.744 17.188-70.067 30.395 4.433 41.673 6.69 84.108 13.702 125.738 40.718 242.03 83.188 483.77 123.316 725.893 5.61 33.799 1.604 69.17 2.412 132.113-164.3-80.74-306.613-150.65-438.69-215.568-37.16-210.437-70.797-400.911-106.349-602.23-199.494 78.834-391.113 154.555-578.868 228.755-128.321-86.98-244.42-165.678-383.043-259.626 307.057-162.055 609.898-256.18 903.892-390.086z" data-name="Path 1"/></svg>
+            
             <Container>
-              <div className="flex flex-wrap">
+              <div className="flex flex-wrap relative z-10">
                 <div className="h-full w-full md:w-1/5">
                   <Link href="/"><a aria-label="Navigate to Home" className="group block py-5 md:py-10 border-b md:border-b-0 md:border-l border-dotted border-off-white border-opacity-50 md:px-8 md:opacity-50 hover:opacity-100 focus:opacity-100 transition ease-in-out duration-500 focus:outline-none">
                     <div className="transition-transform ease-in-out duration-500 md:transform group-hover:-translate-y-12 group-focus:-translate-y-12">
