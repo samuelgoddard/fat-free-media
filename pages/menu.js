9532518bde395149b7ac92e9eced97a6196d773b
Sam Goddard <hello@samgoddard.co.uk>
2021-02-23 17:24:11 +0000

    added autoplay video on hover to menu items

--- pages/menu.js
+++ pages/menu.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { renderMetaTags } from "react-datocms";
+import { renderMetaTags, Image } from "react-datocms";
 import Header from '../components/header'
 import Link from "next/link";
 import Layout from "../components/layout";
@@ -11,7 +11,7 @@ import { motion } from "framer-motion"
 import { fade, reveal, menuMovement, menuMovementOne, menuMovementTwo, menuMovementThree } from "../helpers/transitionHelper"
 import { SmoothScrollProvider } from '../contexts/SmoothScroll.context'
 
-export default function Menu({ data: { site } }) {
+export default function Menu({ data: { site, home, about, work, contact } }) {
   // const metaTags = about.seo.concat(site.favicon);
 
   return (
@@ -36,7 +36,23 @@ export default function Menu({ data: { site } }) {
                     <motion.div variants={menuMovement}>
                       <div className="transition-transform ease-in-out duration-500 md:transform group-hover:-translate-y-12 group-focus:-translate-y-12">
                         
-                        <img src="https://placedog.net/650/900" alt="Placeholder" className="transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block" />
+                        <div className="relative transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block z-0">
+                          <Image
+                            data={{
+                              ...home.menuImage.responsiveImage,
+                              alt: home.menuImage.alt ? home.menuImage.alt : home.menuImage.title,
+                            }}
+                            className="w-full"
+                          />
+
+                          <div className="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0 group-hover:opacity-100 transition ease-in-out duration-300">
+                            <video loop={true} playsInline autoPlay="autoplay" muted className="w-full h-full object-cover z-10">
+                              <source src={ home.menuVideo.video.mp4Url } type="video/mp4" />
+                              
+                              Sorry. Your browser does not support the video tag.
+                            </video>
+                          </div>
+                        </div>
 
                         <motion.div
                           initial="initial"
@@ -51,7 +67,7 @@ export default function Menu({ data: { site } }) {
                           </div>
 
                           <div className="relative overflow-hidden">
-                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">An overview of what we’re about</motion.p>
+                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">{home.menuText}</motion.p>
                           </div>
                         </motion.div>
                       </div>
@@ -62,8 +78,23 @@ export default function Menu({ data: { site } }) {
                   <Link href="/work"><a aria-label="Navigate to Home" className="group block py-5 md:py-10 border-b md:border-b-0 md:border-l border-dotted border-orange-light border-opacity-50 md:px-8 transition ease-in-out duration-500 focus:outline-none text-white hover:text-off-white focus:text-off-white">
                     <motion.div variants={menuMovementOne}>
                       <div className="transition-transform ease-in-out duration-500 md:transform group-hover:-translate-y-12 group-focus:-translate-y-12">
-                        
-                        <img src="https://placedog.net/650/900" alt="Placeholder" className="transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block" />
+                        <div className="relative transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block z-0">
+                          <Image
+                            data={{
+                              ...work.menuImage.responsiveImage,
+                              alt: work.menuImage.alt ? work.menuImage.alt : work.menuImage.title,
+                            }}
+                            className="w-full"
+                          />
+
+                          <div className="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0 group-hover:opacity-100 transition ease-in-out duration-300">
+                            <video loop={true} playsInline autoPlay="autoplay" muted className="w-full h-full object-cover z-10">
+                              <source src={ work.menuVideo.video.mp4Url } type="video/mp4" />
+                              
+                              Sorry. Your browser does not support the video tag.
+                            </video>
+                          </div>
+                        </div>
 
                         <motion.div
                           initial="initial"
@@ -78,7 +109,7 @@ export default function Menu({ data: { site } }) {
                           </div>
 
                           <div className="relative overflow-hidden">
-                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">A selection of our best bits</motion.p>
+                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">{work.menuText}</motion.p>
                           </div>
                         </motion.div>
                       </div>
@@ -89,8 +120,23 @@ export default function Menu({ data: { site } }) {
                   <Link href="/about"><a aria-label="Navigate to Home" className="group block py-5 md:py-10 border-b md:border-b-0 md:border-l border-dotted border-orange-light border-opacity-50 md:px-8 transition ease-in-out duration-500 focus:outline-none text-white hover:text-off-white focus:text-off-white">
                     <motion.div variants={menuMovementTwo}>
                       <div className="transition-transform ease-in-out duration-500 md:transform group-hover:-translate-y-12 group-focus:-translate-y-12">
-                        
-                        <img src="https://placedog.net/650/900" alt="Placeholder" className="transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block" />
+                        <div className="relative transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block z-0">
+                          <Image
+                            data={{
+                              ...about.menuImage.responsiveImage,
+                              alt: about.menuImage.alt ? about.menuImage.alt : about.menuImage.title,
+                            }}
+                            className="w-full"
+                          />
+
+                          <div className="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0 group-hover:opacity-100 transition ease-in-out duration-300">
+                            <video loop={true} playsInline autoPlay="autoplay" muted className="w-full h-full object-cover z-10">
+                              <source src={ about.menuVideo.video.mp4Url } type="video/mp4" />
+                              
+                              Sorry. Your browser does not support the video tag.
+                            </video>
+                          </div>
+                        </div>
 
                         <motion.div
                           initial="initial"
@@ -105,7 +151,7 @@ export default function Menu({ data: { site } }) {
                           </div>
                           
                           <div className="relative overflow-hidden">
-                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">Get to know us a little better</motion.p>
+                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">{about.menuText}</motion.p>
                           </div>
                         </motion.div>
                       </div>
@@ -142,7 +188,23 @@ export default function Menu({ data: { site } }) {
                     <motion.div variants={menuMovementThree}>
                       <div className="transition-transform ease-in-out duration-500 md:transform group-hover:-translate-y-12 group-focus:-translate-y-12">
                         
-                        <img src="https://placedog.net/650/900" alt="Placeholder" className="transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block" />
+                        <div className="relative transition ease-in-out duration-500 opacity-25 group-hover:opacity-100 group-focus:opacity-100 mb-10 transform skew-y-2 hidden md:block z-0">
+                          <Image
+                            data={{
+                              ...contact.menuImage.responsiveImage,
+                              alt: contact.menuImage.alt ? contact.menuImage.alt : contact.menuImage.title,
+                            }}
+                            className="w-full"
+                          />
+
+                          <div className="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0 group-hover:opacity-100 transition ease-in-out duration-300">
+                            <video loop={true} playsInline autoPlay="autoplay" muted className="w-full h-full object-cover z-10">
+                              <source src={ contact.menuVideo.video.mp4Url } type="video/mp4" />
+                              
+                              Sorry. Your browser does not support the video tag.
+                            </video>
+                          </div>
+                        </div>
 
                         <motion.div
                           initial="initial"
@@ -156,7 +218,7 @@ export default function Menu({ data: { site } }) {
                             <motion.h3 variants={reveal} className="text-3xl md:text-3xl lg:text-4xl xl:text-5xl leading-none tracking-tighter mb-0">Contact</motion.h3>
                           </div>
                           <div className="relative overflow-hidden">
-                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">It would be lovely to hear from you</motion.p>
+                            <motion.p variants={reveal} className="font-mono text-sm mb-0 pb-0">{ contact.menutext }</motion.p>
                           </div>
                         </motion.div>
                       </div>
@@ -173,14 +235,75 @@ export default function Menu({ data: { site } }) {
 }
 
 const MENU_QUERY = `
-  query HomePage {
+  query MenuPage {
     site: _site {
       favicon: faviconMetaTags {
         ...metaTagsFragment
       }
     }
+    home {
+      menuImage {
+        responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 720, h: 1000 }) {
+          ...responsiveImageFragment
+        }
+        title
+        alt
+      }
+      menuVideo {
+        video {
+          mp4Url
+        }
+      }
+      menuText
+    }
+    about {
+      menuImage {
+        responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 720, h: 1000 }) {
+          ...responsiveImageFragment
+        }
+        title
+        alt
+      }
+      menuVideo {
+        video {
+          mp4Url
+        }
+      }
+      menuText
+    }
+    work: workLanding {
+      menuImage {
+        responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 720, h: 1000 }) {
+          ...responsiveImageFragment
+        }
+        title
+        alt
+      }
+      menuVideo {
+        video {
+          mp4Url
+        }
+      }
+      menuText
+    }
+    contact {
+      menuImage {
+        responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 720, h: 1000 }) {
+          ...responsiveImageFragment
+        }
+        title
+        alt
+      }
+      menuVideo {
+        video {
+          mp4Url
+        }
+      }
+      menuText
+    }
   }
   ${metaTagsFragment}
+  ${responsiveImageFragment}
 `
 
 export async function getStaticProps() {
