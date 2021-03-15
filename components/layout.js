9d1020d3fc002e1d1ba9df27bc72365328f0322b
Sam Goddard <hello@samgoddard.co.uk>
2021-03-15 13:41:10 +0000

    added canonical tags and tidied up schema

--- components/layout.js
+++ components/layout.js
@@ -1,66 +1,4 @@
-import { useEffect } from "react";
-import Head from "next/head";
-
 export default function Layout({ children, preview }) {
-  
-  // GTAG Analytics
-  // useEffect(() => {
-  //   if (process.env.NODE_ENV === 'development') {
-  //     window.dataLayer = window.dataLayer || []
-      
-  //     function gtag() {
-  //       dataLayer.push(arguments)
-  //     }
-
-  //     gtag('js', new Date())
-  //     gtag('config', 'UA-58771627-1', {
-  //       page_location: window.location.href,
-  //       page_path: window.location.pathname,
-  //       page_title: window.document.title,
-  //     })
-  //   }
-  // }, [])
-
-  const schema = `
-    {
-      "@context":"https://schema.org/",
-      "@type":"Website",
-      "name":"Fat Free Media",
-      "url":"https://hellofatfree.com/",
-      "author": { 
-        "@type":"ProfessionalService",
-        "description":"We write and direct remarkable video for brands and agencies",
-        "url":"https://hellofatfree.com/",
-        "telephone":"+44 115 857 7502",
-        "email":"hello@hellofatfree.com",
-        "image":{
-          "@type":"ImageObject",
-          "url":"https://www.datocms-assets.com/41630/1615305141-nssteaser.jpg?fit=max&fm=jpg&w=1000",
-          "height":560,
-          "width":1000
-        },
-        "contactPoint":[
-          {
-              "@type":"ContactPoint",
-              "contactType":"sales",
-              "telephone":"0115 857 7502"
-          }
-        ],
-        "address":{
-          "@type":"PostalAddress",
-          "addressCountry":"GB",
-          "addressLocality":"Nottingham",
-          "postalCode":"NG1 1HS",
-          "streetAddress":"16 Commerce Square  Lace Market"
-        },
-        "sameAs":[
-          "https://www.instagram.com/hellofatfree",
-          "https://vimeo.com/hellofatfree"
-        ]
-      }
-    }
-  `
-
   return (
     <div>
       { preview && (
@@ -77,17 +15,6 @@ export default function Layout({ children, preview }) {
           </>
         </div>
       )}
-      {/* <Head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=UA-58771627-1`}></script>
-      </Head> */}
-
-      <Head>
-      <script type="application/ld+json" 
-        dangerouslySetInnerHTML={{ 
-          __html: schema
-        }}>
-      </script>
-      </Head>
 
       {children}
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,75 @@ import React from 'react';
 import '../styles/main.css'
 import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+
+  // GTAG Analytics
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV === 'development') {
+  //     window.dataLayer = window.dataLayer || []
+      
+  //     function gtag() {
+  //       dataLayer.push(arguments)
+  //     }
+
+  //     gtag('js', new Date())
+  //     gtag('config', 'UA-58771627-1', {
+  //       page_location: window.location.href,
+  //       page_path: window.location.pathname,
+  //       page_title: window.document.title,
+  //     })
+  //   }
+  // }, [])
+
+  const schema = `
+    {
+      "@context":"https://schema.org/",
+      "@type":"Website",
+      "name":"Fat Free Media",
+      "url":"https://hellofatfree.com/",
+      "image":{
+        "@type":"ImageObject",
+        "url":"https://www.datocms-assets.com/41630/1615815600-ff-logo-schema.png",
+        "height":400,
+        "width":400
+      },
+      "author": { 
+        "@type":"ProfessionalService",
+        "name":"Fat Free Media",
+        "description":"We work with ambitious brands that need courage, clarity & consistency in their video, inside & out.",
+        "url":"https://hellofatfree.com/",
+        "telephone":"+44 115 857 7502",
+        "email":"hello@hellofatfree.com",
+        "image":{
+          "@type":"ImageObject",
+          "url":"https://www.datocms-assets.com/41630/1615815600-ff-logo-schema.png",
+          "height":400,
+          "width":400
+        },
+        "contactPoint":[
+          {
+              "@type":"ContactPoint",
+              "contactType":"sales",
+              "telephone":"0115 857 7502"
+          }
+        ],
+        "address":{
+          "@type":"PostalAddress",
+          "addressCountry":"GB",
+          "addressLocality":"Nottingham",
+          "postalCode":"NG1 1HS",
+          "streetAddress":"16 Commerce Square  Lace Market"
+        },
+        "sameAs":[
+          "https://www.instagram.com/hellofatfree",
+          "https://vimeo.com/hellofatfree"
+        ]
+      }
+    }
+  `
   
   return (
     <>
@@ -14,6 +80,16 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} key={router.asPath} />
         </AnimatePresence>
       </div>
+
+      <Head>
+        <link rel="canonical" href={`https://fat-free.vercel.app${router.asPath}`} />
+        {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=UA-58771627-1`}></script> */}
+        <script type="application/ld+json" 
+          dangerouslySetInnerHTML={{ 
+            __html: schema
+          }}>
+        </script>
+      </Head>
     </>
   );
 }
