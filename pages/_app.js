df4773aef04d1d086a505133a000bb2069b1e9ec
Sam Goddard <hello@samgoddard.co.uk>
2021-03-16 12:46:39 +0000

    GA testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/main.css'
 import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router'
