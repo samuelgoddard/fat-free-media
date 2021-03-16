8172752e0d304812bf77ef51bb0d3baa20a8691b
Sam Goddard <hello@samgoddard.co.uk>
2021-03-16 09:51:51 +0000

    added licensed fonts + redirects

--- next.config.js
+++ next.config.js
@@ -5,4 +5,283 @@ module.exports = {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
   },
+  async redirects() {
+    return [
+      {
+        source: '/about-fat-free-media/',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/onlymotion',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/clomid-for-sale/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/lowest-price-levitra-25mg/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cheapest-ca-ventolin/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/sale-canada-online-cialis-drugs/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-for-women-what-does-it-do/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/buy-levitra-20mg/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/can-i-buy-viagra-at-cvs/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cialis-daily-dose/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cdn-cgi/l/email-protection',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/amoxil-for-sale/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/buy-ca-cialis-super-active-online/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/buy-levitra-kentucky/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/buy-vardenafil-price/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/buy-zithromax-no-prescription/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/canada-female-cialis/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/canada-online-levitra-drugs/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/canadian-priligy-usa-online/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cheapest-propecia-sale-uk/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cialis-5mg-daily-price/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cialis-without-prescription/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/how-long-for-viagra-to-kick-in/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/levitra-philadelphia-online/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/online-tadalafil-canada/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/order-discount-vardenafil/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/order-viagra-price/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/sale-cialis-rx/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/sale-levitra-online-pills-usa/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/shop-priligy-canada/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-cost-cvs/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-pills-for-sale/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-super-fluox-force-usa/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-super-force-usa-online/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/viagra-tablets-for-men/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/where-can-i-buy-viagra/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/services/motion-graphics/',
+        destination: '/disciplines/motion-graphics',
+        permanent: true,
+      },
+      {
+        source: '/services/3d-animation/',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/filmhubwales-anim18',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/nottingham-alternative-statues',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/uon-women-in-engineering',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/ntu-this-is-your-city',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/rab-aw2020',
+        destination: '/work/rab-mixed-media-season-launch',
+        permanent: true,
+      },
+      {
+        source: '/work/uon-research',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/nffc-that-loving-feeling',
+        destination: '/work/nottingham-forest-record-breaking-season-ticket-campaign',
+        permanent: true,
+      },
+      {
+        source: '/work/ntu-nss',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/toyota-system-of-active-stability',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/abrsm-journeys',
+        destination: '/work/abrsm-journeys-app-launch',
+        permanent: true,
+      },
+      {
+        source: '/work/cartwright-pickard-brand-film',
+        destination: '/work/cartwright-pickard-b2b-brand-film',
+        permanent: true,
+      },
+      {
+        source: '/work/center-parcs-i-am-toddler',
+        destination: '/work/centerparcs-toddler-go-pro-amazing-chaos',
+        permanent: true,
+      },
+      {
+        source: '/work/rab-mythic-ultra',
+        destination: '/work/rab-flagship-product-film',
+        permanent: true,
+      },
+      {
+        source: '/work/wattbike-home-advantage',
+        destination: '/work/wattbike-multi-channel-product-launch',
+        permanent: true,
+      },
+      {
+        source: '/work/ec-climate-change',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/work/wattbike-atom-2020',
+        destination: '/work/wattbike-multi-channel-product-launch',
+        permanent: true,
+      },
+      {
+        source: '/work/abrsm-music-theory',
+        destination: '/work/abrsm-we-love-music-theory',
+        permanent: true,
+      }
+    ]
+  },
 }
